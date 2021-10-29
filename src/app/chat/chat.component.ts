2ce3b77c62178ad3f2278656f4075fe42132d6b4
Dumfarth Felix <f.dumfarth@students.htl-leonding.ac.at>
2021-10-29 15:22:41 +0200

    updated frontend to be more responsive

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -14,9 +14,12 @@ export class ChatComponent implements OnInit {
   public messages = [];
   public response: any;
   public sender = 'FE-S-' + Date.now();
-
   private chatService: ChatService;
-
+  private time = 0;
+  public display ;
+  private interval;
+  private minutes: number;
+  private seconds: number;
   constructor(private http: HttpClient, chatService: ChatService) {
     // this.chatService = chatService;
     // this.chatService.connect(this.url);
@@ -31,6 +34,8 @@ export class ChatComponent implements OnInit {
 
   sendMessage(): void {
     if (this.text.replace(/\s/g, '').length) {
+      this.pauseTimer();
+      this.startTimer();
       this.addMessage('me', this.text.trim(), 'sent', 'text');
       // this.chatService.sendMessage(this.text);
       this.response = this.http.post<[{
@@ -45,6 +50,11 @@ export class ChatComponent implements OnInit {
               console.log(dataKey);
               if (dataKey !== 'recipient_id') {
                 this.addMessage('bot', value[dataKey], 'received', dataKey);
+                if (dataKey === 'text'){
+                  if (value[dataKey] === 'Tschüss'){
+                    this.reload();
+                  }
+                }
               }
             }
           }
@@ -55,6 +65,8 @@ export class ChatComponent implements OnInit {
   }
   sendMessageButton(text): void {
     if (text.replace(/\s/g, '').length) {
+      this.pauseTimer();
+      this.startTimer();
       this.addMessage('me', text.trim(), 'sent', 'text');
       // this.chatService.sendMessage(this.text);
       this.response = this.http.post<any>('http://vm07.htl-leonding.ac.at/core/webhooks/rest/webhook',
@@ -64,6 +76,11 @@ export class ChatComponent implements OnInit {
             if (value.hasOwnProperty(dataKey)) {
               if (dataKey !== 'recipient_id') {
                 this.addMessage('bot', value[dataKey], 'received', dataKey);
+                if (dataKey === 'text'){
+                  if (value[dataKey] === 'Tschüss'){
+                    this.reload();
+                  }
+                }
               }
             }
           }
@@ -76,5 +93,41 @@ export class ChatComponent implements OnInit {
   enter($event: any): void {
     $event.preventDefault();
     this.sendMessage();
+  }
+
+  startTimer(): void {
+    console.log('=====>');
+    this.interval = setInterval(() => {
+      if (this.time === 0) {
+        this.time++;
+      } else {
+        this.time++;
+      }
+      this.display = this.transform( this.time);
+      if (this.seconds === 30){
+        this.addMessage('bot', 'Hey, du hast schon seit 30 Sekunden nichts mehr geschrieben, in 30s werde ich unsre Unterhaltung löschen.', 'received', 'text');
+      }
+      if (this.minutes === 1){
+        this.reload();
+      }
+    }, 1000);
+  }
+  transform(value: number): string {
+    this.minutes = Math.floor(value / 60);
+    this.seconds = value - this.minutes * 60;
+    return this.minutes + ':' + (value - this.minutes * 60);
+  }
+  pauseTimer(): void {
+    clearInterval(this.interval);
+    if (this.seconds > 30) {
+      this.messages = this.messages.slice(0, -1);
+    }
+    this.time = 0;
+  }
+
+  reload(): void {
+    this.pauseTimer();
+    this.messages = [];
+    this.sender = 'FE-S-' + Date.now();
   }
 }
